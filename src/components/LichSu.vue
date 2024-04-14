1992ac9669a25dc068782b2f8199671c67d7e2a8
dvTinh99 <dvtinh.it@gmail.com>
2024-04-14 22:58:18 +0700

    update history

--- src/components/LichSu.vue
+++ src/components/LichSu.vue
@@ -81,103 +81,50 @@
                     </tr>
                 </thead>
                 <tbody>
-                    <tr class="odd:bg-white-f8">
-                        <td class="border border-gray-200 py-1 px-4 text-center text-sm">#39617</td>
+                    <tr class="odd:bg-white-f8" v-for="history in histories" >
+                        <td class="border border-gray-200 py-1 px-4 text-center text-sm">#{{ history.id }}</td>
                         <td class="border border-gray-200 py-1 px-4 text-center text-sm">
-                                <span
+                            <span
                                 class="whitespace-nowrap rounded-[.25em] py-0.5 px-2.5 text-[10.5px] font-semibold text-white bg-orange-400 uppercase">
-                                Kunai</span>
+                                Kunai
+                            </span>
                         </td>
-                        <td class="border border-gray-200 py-1 px-4 text-center text-sm">66,228</td>
-                        <td class="border border-gray-200 py-1 px-4 text-center text-sm">951,763,341</td>
-                        <td class="border border-gray-200 py-1 px-4 text-center text-sm">39</td>
-                        <td class="border border-gray-200 py-1 px-4 text-center text-sm"><span
-                                class="whitespace-nowrap rounded-[.25em] py-0.5 px-2.5 text-[10.5px] font-semibold text-white bg-[#5bc0de]">ĐAO</span>
+                        <td class="border border-gray-200 py-1 px-4 text-center text-sm">{{ numberWithCommas(history.xu) }}</td>
+                        <td class="border border-gray-200 py-1 px-4 text-center text-sm">{{ numberWithCommas(history.sum) }}</td>
+                        <td class="border border-gray-200 py-1 px-4 text-center text-sm">{{ numberWithCommas(history.total_result) }}</td>
+                        <td class="border border-gray-200 py-1 px-4 text-center text-sm">
+                            <span class="whitespace-nowrap rounded-[.25em] py-0.5 px-2.5 text-[10.5px] font-semibold text-white" 
+                                :class="{'bg-[#5bc0de]': arrayResult[history.result][0] == 'ĐAO', 'bg-[#f0ad4e]': arrayResult[history.result][0] == 'CUNG', 'bg-[#d9534f]': arrayResult[history.result][0] == 'KIẾM',
+                                    'bg-[#3705fc]': arrayResult[history.result][0] == 'KUNAI',
+                                    'bg-[#5cb85c]': arrayResult[history.result][0] == 'TIÊU',
+                                    'bg-[#67617d]': arrayResult[history.result][0] == 'QUẠT',
+                                    'bg-[#333333]': arrayResult[history.result][0] == '' }"> {{ arrayResult[history.result][0] }}
+                            </span>
                         </td>
                         <td class="border border-gray-200 py-1 px-4 text-center text-sm"><span
-                                class="whitespace-nowrap rounded-[.25em] py-0.5 px-2.5 text-[10.5px] font-semibold text-white bg-[#5cb85c]">TIÊU</span>
+                                class="whitespace-nowrap rounded-[.25em] py-0.5 px-2.5 text-[10.5px] font-semibold text-white" :class="{'bg-[#5bc0de]': arrayResult[history.result][0] == 'ĐAO', 'bg-[#f0ad4e]': arrayResult[history.result][0] == 'CUNG', 'bg-[#d9534f]': arrayResult[history.result][0] == 'KIẾM',
+                                    'bg-[#3705fc]': arrayResult[history.result][1] == 'KUNAI',
+                                    'bg-[#5cb85c]': arrayResult[history.result][1] == 'TIÊU',
+                                    'bg-[#67617d]': arrayResult[history.result][1] == 'QUẠT',
+                                    'bg-[#333333]': arrayResult[history.result][1] == '' }">{{ arrayResult[history.result][1] }}</span>
                         </td>
                         <td class="border border-gray-200 py-1 px-4 text-center text-sm"><span
-                                class="whitespace-nowrap rounded-[.25em] py-0.5 px-2.5 text-[10.5px] font-semibold text-white bg-[#67617d]">QUẠT</span>
+                                class="whitespace-nowrap rounded-[.25em] py-0.5 px-2.5 text-[10.5px] font-semibold text-white" :class="{'bg-[#5bc0de]': arrayResult[history.result][0] == 'ĐAO', 'bg-[#f0ad4e]': arrayResult[history.result][0] == 'CUNG', 'bg-[#d9534f]': arrayResult[history.result][0] == 'KIẾM',
+                                    'bg-[#3705fc]': arrayResult[history.result][2] == 'KUNAI',
+                                    'bg-[#5cb85c]': arrayResult[history.result][2] == 'TIÊU',
+                                    'bg-[#67617d]': arrayResult[history.result][2] == 'QUẠT',
+                                    'bg-[#333333]': arrayResult[history.result][2] == '' }">{{ arrayResult[history.result][2] }}</span>
                         </td>
                         <td class="border border-gray-200 py-1 px-4 text-center text-sm"><span
-                                class="whitespace-nowrap rounded-[.25em] py-0.5 px-2.5 text-[10.5px] font-semibold text-white bg-[#b8a500]">9</span>
+                                class="whitespace-nowrap rounded-[.25em] py-0.5 px-2.5 text-[10.5px] font-semibold text-white" :class="arrayColorSC[history.result]">{{ history.result }}</span>
                         </td>
                         <td class="bor0er-r whitespace-nowrap border-gray-100 py-1 px-4 text-sm">
-                            <p><strong class="inline-block w-16">Bắt đầu:</strong> <span>26-03-2024 14:02:56</span></p>
-                            <p><strong class="inline-block w-16">Kết thúc:</strong> <span>26-03-2024 14:05:50</span></p>
+                            <p><strong class="inline-block w-16">Bắt đầu:</strong> <span>{{ moment(history.created_at) }}</span></p>
+                            <p><strong class="inline-block w-16">Kết thúc:</strong> <span>{{ moment(history.created_at, true) }}</span></p>
                         </td>
                         <td class="border border-gray-200 py-1 px-4 text-center text-sm"><a
                                 class="flex items-center whitespace-nowrap rounded-sm border border-sky-500 bg-sky-400 py-0.5 px-2.5 text-sm text-white hover:bg-sky-500"
                                 href="/?view-key=f4159f4cbdada3e6cf6ebe93b0ae4b98" target="_blank"><svg
-                                    stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 576 512"
-                                    class="mr-1 inline" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
-                                    <path
-                                        d="M572.52 241.4C518.29 135.59 410.93 64 288 64S57.68 135.64 3.48 241.41a32.35 32.35 0 0 0 0 29.19C57.71 376.41 165.07 448 288 448s230.32-71.64 284.52-177.41a32.35 32.35 0 0 0 0-29.19zM288 400a144 144 0 1 1 144-144 143.93 143.93 0 0 1-144 144zm0-240a95.31 95.31 0 0 0-25.31 3.79 47.85 47.85 0 0 1-66.9 66.9A95.78 95.78 0 1 0 288 160z">
-                                    </path>
-                                </svg>Xem</a></td>
-                    </tr>
-                    <tr class="odd:bg-white-f8">
-                        <td class="border border-gray-200 py-1 px-4 text-center text-sm">#39616</td>
-                        <td class="border border-gray-200 py-1 px-4 text-center text-sm"><span
-                                class="whitespace-nowrap rounded-[.25em] py-0.5 px-2.5 text-[10.5px] font-semibold text-white bg-orange-400 uppercase">Kunai</span>
-                        </td>
-                        <td class="border border-gray-200 py-1 px-4 text-center text-sm">69,334</td>
-                        <td class="border border-gray-200 py-1 px-4 text-center text-sm">151,168,080</td>
-                        <td class="border border-gray-200 py-1 px-4 text-center text-sm">30</td>
-                        <td class="border border-gray-200 py-1 px-4 text-center text-sm"><span
-                                class="whitespace-nowrap rounded-[.25em] py-0.5 px-2.5 text-[10.5px] font-semibold text-white bg-[#f0ad4e]">CUNG</span>
-                        </td>
-                        <td class="border border-gray-200 py-1 px-4 text-center text-sm"><span
-                                class="whitespace-nowrap rounded-[.25em] py-0.5 px-2.5 text-[10.5px] font-semibold text-white bg-[#d9534f]">KIẾM</span>
-                        </td>
-                        <td class="border border-gray-200 py-1 px-4 text-center text-sm"><span
-                                class="whitespace-nowrap rounded-[.25em] py-0.5 px-2.5 text-[10.5px] font-semibold text-white bg-[#3705fc]">KUNAI</span>
-                        </td>
-                        <td class="border border-gray-200 py-1 px-4 text-center text-sm"><span
-                                class="whitespace-nowrap rounded-[.25em] py-0.5 px-2.5 text-[10.5px] font-semibold text-white bg-[#f0ad4e]">0</span>
-                        </td>
-                        <td class="bor0er-r whitespace-nowrap border-gray-100 py-1 px-4 text-sm">
-                            <p><strong class="inline-block w-16">Bắt đầu:</strong> <span>26-03-2024 13:59:04</span></p>
-                            <p><strong class="inline-block w-16">Kết thúc:</strong> <span>26-03-2024 14:02:56</span></p>
-                        </td>
-                        <td class="border border-gray-200 py-1 px-4 text-center text-sm"><a
-                                class="flex items-center whitespace-nowrap rounded-sm border border-sky-500 bg-sky-400 py-0.5 px-2.5 text-sm text-white hover:bg-sky-500"
-                                href="/?view-key=3c4d78d9fa482027c1d5e9758e044e87" target="_blank"><svg
-                                    stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 576 512"
-                                    class="mr-1 inline" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
-                                    <path
-                                        d="M572.52 241.4C518.29 135.59 410.93 64 288 64S57.68 135.64 3.48 241.41a32.35 32.35 0 0 0 0 29.19C57.71 376.41 165.07 448 288 448s230.32-71.64 284.52-177.41a32.35 32.35 0 0 0 0-29.19zM288 400a144 144 0 1 1 144-144 143.93 143.93 0 0 1-144 144zm0-240a95.31 95.31 0 0 0-25.31 3.79 47.85 47.85 0 0 1-66.9 66.9A95.78 95.78 0 1 0 288 160z">
-                                    </path>
-                                </svg>Xem</a></td>
-                    </tr>
-                    <tr class="odd:bg-white-f8">
-                        <td class="border border-gray-200 py-1 px-4 text-center text-sm">#39615</td>
-                        <td class="border border-gray-200 py-1 px-4 text-center text-sm"><span
-                                class="whitespace-nowrap rounded-[.25em] py-0.5 px-2.5 text-[10.5px] font-semibold text-white bg-orange-400 uppercase">Kunai</span>
-                        </td>
-                        <td class="border border-gray-200 py-1 px-4 text-center text-sm">89,873</td>
-                        <td class="border border-gray-200 py-1 px-4 text-center text-sm">447,069,656</td>
-                        <td class="border border-gray-200 py-1 px-4 text-center text-sm">47</td>
-                        <td class="border border-gray-200 py-1 px-4 text-center text-sm"><span
-                                class="whitespace-nowrap rounded-[.25em] py-0.5 px-2.5 text-[10.5px] font-semibold text-white bg-[#5bc0de]">ĐAO</span>
-                        </td>
-                        <td class="border border-gray-200 py-1 px-4 text-center text-sm"><span
-                                class="whitespace-nowrap rounded-[.25em] py-0.5 px-2.5 text-[10.5px] font-semibold text-white bg-[#5cb85c]">TIÊU</span>
-                        </td>
-                        <td class="border border-gray-200 py-1 px-4 text-center text-sm"><span
-                                class="whitespace-nowrap rounded-[.25em] py-0.5 px-2.5 text-[10.5px] font-semibold text-white bg-[#67617d]">QUẠT</span>
-                        </td>
-                        <td class="border border-gray-200 py-1 px-4 text-center text-sm"><span
-                                class="whitespace-nowrap rounded-[.25em] py-0.5 px-2.5 text-[10.5px] font-semibold text-white bg-[#00b093]">7</span>
-                        </td>
-                        <td class="bor0er-r whitespace-nowrap border-gray-100 py-1 px-4 text-sm">
-                            <p><strong class="inline-block w-16">Bắt đầu:</strong> <span>26-03-2024 13:54:24</span></p>
-                            <p><strong class="inline-block w-16">Kết thúc:</strong> <span>26-03-2024 13:59:04</span></p>
-                        </td>
-                        <td class="border border-gray-200 py-1 px-4 text-center text-sm"><a
-                                class="flex items-center whitespace-nowrap rounded-sm border border-sky-500 bg-sky-400 py-0.5 px-2.5 text-sm text-white hover:bg-sky-500"
-                                href="/?view-key=ec133e011d59115f6ec97c5888b570fe" target="_blank"><svg
                                     stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 576 512"
                                     class="mr-1 inline" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
                                     <path
@@ -209,18 +156,71 @@
 
 <script>
 import Master from "./Master.vue";
+import axios from "axios";
+import moment from "moment";
+
+const API_URL = import.meta.env.VITE_WEB_API;
 export default {
     components: {
         Master
     },
     data() {
         return {
-
+            histories : [],
+            arrayColorSC: [
+                "bg-[#f0ad4e]",
+                "bg-[#5bc0de]", // 1 chua biet
+                "bg-[#5cb85c]",
+                "bg-[#d9534f]", //3
+                "bg-[#3705fc]", // 4
+                "bg-[#67617d]", // 5
+                "bg-[#cf02a9]", //6
+                "bg-[#00b093]", //7
+                "bg-[#7400bd]", //8
+                "bg-[#b8a500]", // 9
+            ],
+            arrayResult: [
+                ["CUNG", "KIẾM", "KUNAI"], //0
+                ["ĐAO", "KIẾM", ""], //1
+                ["CUNG", "KIẾM", ""], //2
+                ["ĐAO", "KIẾM", ""], //3
+                ["CUNG", "KIẾM", "KUNAI"], //4
+                ["ĐAO", "TIÊU", "QUẠT"], //5
+                ["CUNG", "TIÊU", ""], //6
+                ["ĐAO", "TIÊU", ""], //7
+                ["CUNG", "TIÊU", ""], //8
+                ["ĐAO", "TIÊU", "QUẠT"], //9
+            ],
+            CDResult: ["C", "Đ", "C", "Đ", "C", "Đ", "C", "Đ", "C", "Đ"],
+            TKResult: ["K", "K", "K", "K", "K", "T", "T", "T", "T", "T"],
+            UQResult: ["U", "", "U", "", "U", "Q", "", "Q", "", "Q"],
+            classObject: {
+                active: true,
+                'text-danger': false
+            }
         }
     },
-    mounted() {
+    async mounted() {
+        const data = await axios.get(API_URL + '/history/all').then(rs => {
+            return rs.data.histories;
+        });
+        this.histories = data;
+        console.log('data', data);
+        
         console.log('lich su ne', this.$route.fullPath);
     },
+    methods: {
+        numberWithCommas(x) {
+         return x.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",");
+        },
+        moment: function (date, isEnd) {
+            if (isEnd) {
+                return moment(date).add(2, 'm').format('MM-DD-YYYY h:mm:ss');  
+            };
+            return moment(date).format('MM-DD-YYYY h:mm:ss');
+        }
+    },
+
 
 }
 </script>
